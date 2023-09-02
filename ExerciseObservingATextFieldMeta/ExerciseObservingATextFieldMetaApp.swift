ed68246de77637ed60802c1b8bd7d6f3c05527c1
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-02 14:22:56 -0600

    Initial Commit

---
+++ ExerciseObservingATextFieldMeta/ExerciseObservingATextFieldMetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciseObservingATextFieldMetaApp.swift
+//  ExerciseObservingATextFieldMeta
+//
+//  Created by Mitch Andrade on 9/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ExerciseObservingATextFieldMetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
